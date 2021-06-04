b373fdc928eb16dc19c0b78499b7a704aac44912
neveditagit <goodlucknivedita@gmail.com>
2021-06-04 12:36:52 +0000

    Commit Lily

---
+++ src/main/java/myautotest_app/myautotest_app/TestLily.java
@@ -0,0 +1,11 @@
+package myautotest_app.myautotest_app;
+
+public class TestLily {
+
+	public static void method1() 
+	{
+		System.out.println("Test Method");
+	}
+	
+	
+}
--- src/test/java/myautotest_app/myautotest_app/AppTest.java
+++ src/test/java/myautotest_app/myautotest_app/AppTest.java
@@ -3,18 +3,22 @@ package myautotest_app.myautotest_app;
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
-
+import myautotest_app.myautotest_app.TestLily;
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
+public class AppTest extends TestCase 
 {
     /**
      * Create the test case
      *
      * @param testName name of the test case
      */
+	
+	TestLily  testLily;// = new TestLily();
+
+	
+	
     public AppTest( String testName )
     {
         super( testName );
@@ -23,6 +27,7 @@ public class AppTest
     /**
      * @return the suite of tests being tested
      */
+    
     public static Test suite()
     {
         return new TestSuite( AppTest.class );
